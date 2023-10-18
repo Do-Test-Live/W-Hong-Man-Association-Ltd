bbb1d082b6151e085704964442737ee6eda595fc
Monoget <monoget2@gmail.com>
2023-10-18 13:00:02 +0600

    update all

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,3 +9,19 @@ $(document).ready(function () {
         }
     });
 });
+
+
+let scrollToTopButton = document.getElementById("hmal-scrollToTop");
+
+window.onscroll = function() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scrollToTopButton.style.display = "block";
+    } else {
+        scrollToTopButton.style.display = "none";
+    }
+};
+
+scrollToTopButton.onclick = function() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+};
