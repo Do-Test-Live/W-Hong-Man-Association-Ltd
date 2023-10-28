c9305316fa8cf2f5cb7dce56d05b3478e24353d8
Monoget <monoget2@gmail.com>
2023-10-28 13:15:22 +0600

    update

---
+++ admin/include/sidebar.php
@@ -0,0 +1,37 @@
+<!-- Sidebar -->
+<ul class="navbar-nav bg-gradient-dark sidebar sidebar-dark accordion" id="accordionSidebar">
+
+    <!-- Sidebar - Brand -->
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="dashboard.php">
+        <div class="sidebar-brand-icon rotate-n-15">
+            <i class="fas fa-laugh-wink"></i>
+        </div>
+        <div class="sidebar-brand-text mx-3">HMAL</div>
+    </a>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider my-0">
+
+    <!-- Nav Item - Dashboard -->
+    <li class="nav-item active">
+        <a class="nav-link" href="dashboard.php">
+            <i class="fas fa-fw fa-tachometer-alt"></i>
+            <span>Dashboard</span></a>
+    </li>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider">
+
+    <!-- Heading -->
+    <div class="sidebar-heading">
+        Data
+    </div>
+
+    <li class="nav-item">
+        <a class="nav-link" href="agreement-data.php">
+            <i class="fas fa-fw fa-cog"></i>
+            <span>Agreement Data</span>
+        </a>
+    </li>
+</ul>
+<!-- End of Sidebar -->
